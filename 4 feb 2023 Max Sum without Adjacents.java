6cc8b130630cc1e27e25f9195b9b0855c52bc788
mohammadfaisalkhan786 <117020639+mohammadfaisalkhan786@users.noreply.github.com>
2023-02-04 07:23:59 +0530

    Create 4 feb 2023 Max Sum without Adjacents.java

---
+++ 4 feb 2023 Max Sum without Adjacents.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int ans = new Solution().findMaxSum(arr, n);
+            out.println(ans);
+        }
+        out.close();
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int findMaxSum(int arr[], int n) {
+        int ans=0,max=0,max1=0;
+        for(int i=0;i<n;i++) {
+            int val = arr[i]+max;
+            max = Math.max(max,max1);
+            max1 = Math.max(max1,val);
+            ans = Math.max(ans,val);
+        }
+        return ans;
+    }
+}
