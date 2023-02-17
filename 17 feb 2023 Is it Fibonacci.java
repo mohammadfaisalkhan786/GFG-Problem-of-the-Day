014ea718c5d679893eaa30f9fd301406f2799031
mohammadfaisalkhan786 <117020639+mohammadfaisalkhan786@users.noreply.github.com>
2023-02-17 09:55:43 +0530

    Create 17 feb 2023 Is it Fibonacci.java

---
+++ 17 feb 2023 Is it Fibonacci.java
@@ -0,0 +1,128 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+    static FastIO f;
+    
+    public static void main(String args[]) throws IOException
+    {
+        f = new FastIO();
+        
+        int t = f.nextInt();
+        
+        while(t-->0)
+        {
+            int N = f.nextInt(), K = f.nextInt();
+            ArrayList<Long> GeekNum = new ArrayList<>();
+            
+            for(int i = 0; i < K; i++)
+                GeekNum.add(f.nextLong());
+            
+            f.out(Solution.solve(N, K, GeekNum) + "\n");
+        }
+        
+        f.flush();
+    }
+}
+
+class FastIO
+{
+	BufferedReader br;
+	PrintWriter bw, be;
+	StringTokenizer st;
+
+	public FastIO()
+	{
+		br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new PrintWriter(System.out);
+		be = new PrintWriter(System.err);
+		st = new StringTokenizer("");
+	}
+
+	private void read() throws IOException
+	{
+		st = new StringTokenizer(br.readLine());
+	}
+
+	public String nextLine() throws IOException
+	{
+		return br.readLine();
+	}
+
+	public String next() throws IOException
+	{
+		while(!st.hasMoreTokens())
+			read();
+		return st.nextToken();
+	}
+
+	public int nextInt() throws IOException
+	{
+		return Integer.parseInt(next());
+	}
+
+	public long nextLong() throws IOException
+	{
+		return Long.parseLong(next());
+	}
+
+	public float nextFloat() throws IOException
+	{
+		return Float.parseFloat(next());
+	}
+
+	public double nextDouble() throws IOException
+	{
+		return Double.parseDouble(next());
+	}
+
+	public char nextChar() throws IOException
+	{
+		return next().charAt(0);
+	}
+
+	public void out(String s) throws IOException
+	{
+		bw.print(s);
+	}
+
+	public void flush() throws IOException
+	{
+		bw.flush();
+		be.flush();
+	}
+
+	public void err(String s) throws IOException
+	{
+		be.print(s);
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static long solve(int N, int K, ArrayList<Long> GetNum) {
+        long arr[]=new long[N];
+        int i=0,j=0;
+        long sum=0;
+        for( j=0; j < N && j < GetNum.size();j++) {
+            arr[j] = GetNum.get(j);
+            sum += GetNum.get(j);
+        }
+        while(j < N)
+        {
+            arr[j]=sum;
+            sum-=arr[i];
+            sum+=arr[j];
+            i++;
+            j++;
+        }
+        return arr[N-1];
+    }
+}
