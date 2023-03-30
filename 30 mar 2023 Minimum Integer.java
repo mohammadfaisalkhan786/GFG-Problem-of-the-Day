4f86b06bfe02c71005adc46eb38b9b8ec995644d
mohammadfaisalkhan786 <117020639+mohammadfaisalkhan786@users.noreply.github.com>
2023-03-30 08:54:19 +0530

    Create 30 mar 2023 Minimum Integer.java

---
+++ 30 mar 2023 Minimum Integer.java
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int N;
+            N = Integer.parseInt(br.readLine());
+            
+            
+            int[] A = IntArray.input(br, N);
+            
+            Solution obj = new Solution();
+            int res = obj.minimumInteger(N, A);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static int minimumInteger(int n, int[] a) {
+        long sum = 0;
+        for(int val:a)
+            sum += (long)val;
+        long b = sum/n;
+        long temp = 0;
+        if(sum==n*b)
+        temp = b;
+        else temp = b+1;
+        long ans = Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){
+            if(a[i]>=temp)  ans = Math.min(ans,a[i]);
+        }
+        return (int)ans;
+    }
+}
